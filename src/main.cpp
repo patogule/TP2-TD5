2430a60e9512f8d71d5af05135e8efdd585d851d
valenpancaldi <126804155+valenpancaldi@users.noreply.github.com>
2023-06-17 22:13:08 -0300

    relocate

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <vector>
+using namespace std;
+
+
 
 int heuristica_vmc(int m){
 
@@ -9,9 +13,46 @@ int heuristica_vmc(int m){
 }
 
 
-int relocate(std::string filename){
-    
+int relocate(string filenamee, vector<int> capacidades) {
+    string filename(filenamee);
+    vector<string> lines;
+    string line;
 
+    ifstream input_file(filename);
+    if (!input_file.is_open()) {
+        cout << "Could not open the file - '"
+             << filename << "'" << endl;
+        return EXIT_FAILURE;
+    }
+
+    while (getline(input_file, line)) {
+        cout << line;
+        lines.push_back(line);
+    }
+
+    vector<int> capacidad_disponible;
+    for (size_t i = 0; i < lines.size(); ++i) {
+    const string& line = lines[i];
+    int sum = 0;
+    string number_str;
+    for (char c : line) {
+        if (isdigit(c)) {
+            number_str += c;
+        } else if (!number_str.empty()) {
+            sum += stoi(number_str);
+            number_str.clear();
+        }
+    }
+    if (!number_str.empty()) {
+        sum += stoi(number_str);
+    }
+    capacidad_disponible[i] = capacidades[i] - sum;
+    cout << "Sum of numbers in line " << i << ": " << sum << "capacidad disponible: " << capacidad_disponible[i] << endl;
+    }
+
+
+    input_file.close();
+    return EXIT_SUCCESS;
 }
 
 
@@ -19,21 +60,8 @@ int main(int argc, char** argv) {
     std::string filename = "instances/gap/gap_a/a05100";
     std::cout << "Reading file " << filename << std::endl;
 
-    // Abrir el archivo
-    std::ifstream file(filename);
-    if (file.is_open()) {
-        // Leer la primera línea
-        std::string firstLine;
-        std::getline(file, firstLine);
-
-        // Imprimir la primera línea
-        std::cout << "First line: " << firstLine << std::endl;
-
-        // Cerrar el archivo
-        file.close();
-    } else {
-        std::cout << "Error opening file" << std::endl;
-    }
+    vector<int> cap = {30,7,20};
+    relocate("resultados.txt", cap);
 
     return 0;
 }
